fcf5687d176f64a7629eaa945297d5946624792d
Thulio Leal <thulioleal.silva@gmail.com>
2026-02-23 20:26:53 -0300

    Update Configuracoes.tsx

--- src/pages/Configuracoes.tsx
+++ src/pages/Configuracoes.tsx
@@ -403,8 +403,8 @@ function ConfiguracoesContent() {
         </motion.div>
 
         {/* Settings Form */}
-        <motion.div variants={fadeInUp} className="grid gap-6 lg:grid-cols-4">
-          <Card className="lg:col-span-4 border border-blue-200 shadow-lg bg-white">
+        <motion.div variants={fadeInUp} className="grid gap-6 lg:span-cols-4">
+          <Card className="lg:col-span-3 border border-blue-200 shadow-lg bg-white">
             <CardHeader className="border-b border-blue-200 bg-blue-50">
               <CardTitle className="text-xl text-gray-900">
                 {isCreating ? 'Nova Configuração' : `Editar: ${selectedSettings?.name}`}
