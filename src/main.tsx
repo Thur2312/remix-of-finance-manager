b27081a9485aee95dae721be234acf80290f1ad1
Thulio Leal <thulioleal.silva@gmail.com>
2026-04-22 23:43:55 -0300

    ✨ feat: inserção do botão de alteração de tema

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 import { ErrorBoundary } from "@/components/ErrorBoundary";
+import { ThemeProvider } from "./contexts/ThemeContext";
 
 createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
-    <ErrorBoundary>
-      <App />
-    </ErrorBoundary>
-  </BrowserRouter>
+  <ThemeProvider>
+    <BrowserRouter>
+      <ErrorBoundary>
+        <App />
+      </ErrorBoundary>
+    </BrowserRouter>
+  </ThemeProvider>
 );
